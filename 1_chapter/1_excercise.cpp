7d4194e6339f098fed4e4eaa11b5605c43e15e49
soleilset <diegollnnm@gmail.com>
2025-01-06 17:53:27 -0500

    Update 1_chapter

---
+++ 1_chapter/1_excercise.cpp
@@ -0,0 +1,12 @@
+// En este programa queremos imprimir en pantalla el mensaje:
+// "Hello, programming!"
+// "Here we go!""
+
+# include<iostream>
+
+int main()
+{
+    std::cout << "Hello, programming!" << std::endl;
+    std::cout << "Here we go!" << std::endl;
+    return 0;
+}
