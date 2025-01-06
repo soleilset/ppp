fbbe99c7e6d0ea4238665f9577f089423557bd0c
soleilset <diegollnnm@gmail.com>
2025-01-06 10:28:33 -0500

    Mis primeros pasos

---
+++ other_codes/triangle.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+int main()
+{
+    std::cout << "          /|" << std::endl;
+    std::cout << "         / |" << std::endl;
+    std::cout << "        /  |" << std::endl;
+    std::cout << "       /   |" << std::endl;
+    std::cout << "      /    |" << std::endl;
+    std::cout << "     /__ __|" << std::endl;
+
+    std::cout << " ________________" << std::endl;
+    std::cout << "|                |" << std::endl;
+    std::cout << "|                |" << std::endl;
+    std::cout << "|                |" << std::endl;
+    std::cout << "|                |" << std::endl;
+    std::cout << "|________________|" << std::endl;
+
+    return 0;
+}
